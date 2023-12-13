4c3717bccaee7403cfaa412d6bb36d0c3da38323
unknown <taeho8199@gmail.com>
2023-12-13 14:24:22 +0900

    ;

--- sample/Hook/smpl-useModal.tsx
+++ sample/Hook/smpl-useModal.tsx
@@ -1,11 +1,36 @@
 import { useModal } from "@/com/hooks";
 import { Button } from "@/com/components";
 
+// export type ModalProps = {
+//     id?: string;
+//     content?: React.ReactNode;
+//     backdrop?: boolean;
+//     size?: keyof typeof MODAL_SIZES;
+//     onConfirm?: () => void;
+//     onCancel?: () => void;
+// };
+
 export const SampleUseModal = () => {
     const modal = useModal();
 
     const withMessage = () => {
         modal.openModal({ content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry" });
+    };
+
+    const withOnConfirm = () => {
+        modal.openModal({ onConfirm: () => alert("confirm") });
+    };
+
+    const withOnCancel = () => {
+        modal.openModal({ onCancel: () => alert("cancel") });
+    };
+
+    const withoutBackdrop = () => {
+        modal.openModal({ backdrop: false });
+    };
+
+    const withSize = () => {
+        modal.openModal({ onCancel: () => alert("cancel") });
     };
 
     const withComponent = () => {
@@ -14,8 +39,11 @@ export const SampleUseModal = () => {
 
     return (
         <div>
-            <Button onClick={withMessage}>메세지 모달 열기</Button>
-            <Button onClick={withComponent}>컴포넌트 모달 열기</Button>
+            <Button onClick={withMessage}>기본</Button>
+            <Button onClick={withOnConfirm}>onConfirm handler 추가</Button>
+            <Button onClick={withOnCancel}>onCancel handler 추가</Button>
+            <Button onClick={withoutBackdrop}>backdrop 제거</Button>
+            <Button onClick={withComponent}>컴포넌트 모달</Button>
         </div>
     );
 };
