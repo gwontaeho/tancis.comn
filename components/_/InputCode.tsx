23b850b5bf0f43367951c813f09dc61c059e669d
claud-bae <claud6998@gmail.com>
2023-12-15 11:18:41 +0900

    ;

--- components/_/InputCode.tsx
+++ components/_/InputCode.tsx
@@ -9,7 +9,8 @@ type InputCodeProps = {
     name?: string;
     value?: any;
     comnCd?: string;
-    minLength?: number;
+    area?: string;
+    maxLength?: number;
     control?: Control;
     onChange?: (...args: any) => void;
 };
@@ -17,6 +18,7 @@ type InputCodeProps = {
 const InputCodeMain = (props: InputCodeProps) => {
     const keywordInput = React.useRef<HTMLInputElement>(null);
     const cdVldValNmInput = React.useRef<HTMLInputElement>(null);
+    const _keyword = React.useRef<any>(null);
 
     React.useEffect(() => {
         if (!props.value) return;
@@ -25,39 +27,81 @@ const InputCodeMain = (props: InputCodeProps) => {
     }, [props.value]);
 
     const handleChange = lodash.debounce(async (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (!e.target.value) return;
+        if (!e.target.value) {
+            if (cdVldValNmInput.current) cdVldValNmInput.current.value = "";
+            return;
+        }
         getComnCd(e.target.value);
     }, 500);
 
     const getComnCd = async (keyword: string) => {
-        if (!props.comnCd) return;
-
-        if (props.minLength !== undefined && keyword.length < props.minLength) {
-            if (keywordInput.current) keywordInput.current.value = keyword;
+        if (!props.area && !props.comnCd) return;
+        if (props.maxLength !== undefined && keyword.length < props.maxLength) {
+            if (cdVldValNmInput.current) cdVldValNmInput.current.value = "";
             return;
         }
 
         try {
+            _keyword.current = keyword;
             const {
                 data: { content },
-            } = await api.get(
-                `http://singlewindow.info:9010/ptl-com/comn/comn-cds?size=1&comnCd=${props.comnCd}&cdVldVal=${keyword}`
-            );
+            } = await api.get(getCodeUrl(keyword));
 
-            if (!content[0]) return;
-            if (cdVldValNmInput.current) cdVldValNmInput.current.value = content[0].cdVldValNm;
-            if (keywordInput.current) keywordInput.current.value = content[0].cdVldVal;
+            if (!content[0]) {
+                if (cdVldValNmInput.current) cdVldValNmInput.current.value = "";
+                return;
+            }
+            if (cdVldValNmInput.current) cdVldValNmInput.current.value = getCodeValue(content[0]);
+            if (keywordInput.current) keywordInput.current.value = getKeyword(content[0]);
 
             if (!props.onChange) return;
             props.onChange(content[0].cdVldVal);
         } catch (error) {}
     };
 
+    const getCodeUrl = (keyword: string) => {
+        if (!props.area || props.area === "comnCd") {
+            return `/ptl-com/comn/comn-cds?size=1&comnCd=${props.comnCd}&cdVldVal=${keyword}`;
+        } else if (props.area === "cntyCd") {
+            return `/ptl-com/comn/cnty-cds?size=1&cntyCd=${keyword}`;
+        } else if (props.area === "currCd") {
+            return `/ptl-com/comn/curr-cds?size=1&currCd=${keyword}`;
+        }
+        return "";
+    };
+
+    const getKeyword = (content: any) => {
+        if (!props.area || props.area === "comnCd") {
+            return content.cdVldVal;
+        } else if (props.area === "cntyCd") {
+            return content.cntyCd;
+        } else if (props.area === "currCd") {
+            return content.currCd;
+        }
+        return "";
+    };
+
+    const getCodeValue = (content: any) => {
+        if (!props.area || props.area === "comnCd") {
+            return content.cdVldValNm;
+        } else if (props.area === "cntyCd") {
+            return content.cntyNm;
+        } else if (props.area === "currCd") {
+            return content.currNm;
+        }
+        return "";
+    };
+
     const handleClickSearch = () => {};
 
     return (
         <div className="flex">
-            <input ref={keywordInput} className="input rounded-r-none flex-1" onChange={handleChange} />
+            <input
+                ref={keywordInput}
+                className="input rounded-r-none flex-1"
+                onChange={handleChange}
+                maxLength={props.maxLength}
+            />
             <button className="button border-x-0 rounded-none" onClick={handleClickSearch}>
                 <Icon icon="search" size="xs" />
             </button>
