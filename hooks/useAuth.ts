d010141c1ada88a1f90430ecb1acdbfa11375f64
claud-bae <claud6998@gmail.com>
2024-01-02 18:24:00 +0900

    ;

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,3 +1,12 @@
 export const useAuth = () => {
-    return {}
-}
+    const auth: { [key: string]: any } = {
+        tin: "111111111",
+    };
+
+    const get = (key: string): any => {
+        return auth[key];
+    };
+    return {
+        get,
+    };
+};
