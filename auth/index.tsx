b8d648439c5b886d8eccc2f8e442dc3596a1f442
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-01-04 09:31:10 +0900

    ;

--- auth/index.tsx
+++ auth/index.tsx
@@ -6,6 +6,7 @@ const AuthProvider = ({ children }: { children?: React.ReactNode }) => {
     }, []);
 
     const authenticate = () => {};
+    console.log("asd");
 
     return <>{children}</>;
 };
