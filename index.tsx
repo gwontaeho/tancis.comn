1128b6ca611905cb4c3801d553862d54177ead96
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-01-04 16:58:28 +0900

    ;

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecoilProvider from "@/comn/features/recoil";
 import { SampleRoutes } from "@/comn/sample/smpl-Routes";
 import { SampleMain } from "@/comn/sample/smpl-Main";
-import { Layout } from "@/comn/layouts";
+import { Layout } from "@/comn/features/layouts";
 import { TancisRoutes } from "@/tra/tancis/Routes";
 import { CommonModal, CommonToast } from "@/comn/components/_";
 import { Main } from "@/tra/tancis/Main";
