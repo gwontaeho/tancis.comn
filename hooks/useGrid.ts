7da5b0a85d8b7486e52edc870cbec3f98329df21
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-02-02 10:19:49 +0900

    ;

--- hooks/useGrid.ts
+++ hooks/useGrid.ts
@@ -20,6 +20,7 @@ export type TGridSchema = {
         id?: string;
         show?: boolean;
         colspan?: number;
+        width?: string | number;
         cells: {
             width?: string | number;
             header?: string;
