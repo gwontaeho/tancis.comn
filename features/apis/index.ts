5d9a19d1e38931588ae753c4c647dc4ff5393ca9
claud-bae <claud6998@gmail.com>
2024-02-07 08:51:59 +0900

    ;

--- features/apis/index.ts
+++ features/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_COMN });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_PTLI });
 
 api.interceptors.request.use(
     (config) => {
