d73df26b979afc9f5ecd24e282b761ca05c8eb81
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-03-04 18:14:09 +0900

    ;

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,6 +1,10 @@
 import { useRecoilState } from "recoil";
 import { themeState } from "@/comn/features/recoil";
 
+/**
+ *
+ * @returns
+ */
 export const useTheme = () => {
     const [theme, setTheme] = useRecoilState(themeState);
     return { theme, setTheme };
